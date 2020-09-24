2dd4116639e713c5d38b68bfabc2651e9ba3ec2a
윤정인 <claire@yunjeong-in-ui-MacBookAir.local>
2020-09-24 16:25:02 +0900

    Initial Commit

---
+++ wedding.js
@@ -0,0 +1,7 @@
+// gsap.from("h1.main-title", { duration: 0.75, y: -10, opacity: 0 });
+// gsap.from("h2.date", { duration: 0.75, y: -10, opacity: 0 });
+
+// var tl = gsap.timeline();
+
+// tl.to("h1.main-title", { duration: 0.75, y: -10, opacity: 0 });
+// tl.to("h2.date", { duration: 0.75, y: -10, opacity: 0 });
