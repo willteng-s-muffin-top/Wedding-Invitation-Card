29f69c7e78ead5121acac6ba5b5b93b2306f662d
Will <rlagmlckd@gmail.com>
2020-11-03 21:07:29 +0900

    Updates

---
+++ precache-manifest.ec1ce924ab63570d7374003470e33771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1fa779fd6e4986c3a76fafb7a91de3f",
+    "url": "/Wedding-Invitation-Card/index.html"
+  },
+  {
+    "revision": "3e0786e4f98849e46d47",
+    "url": "/Wedding-Invitation-Card/static/css/main.5ae064d7.chunk.css"
+  },
+  {
+    "revision": "c3a2c0832786a9fbe478",
+    "url": "/Wedding-Invitation-Card/static/js/2.3d109e4d.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/Wedding-Invitation-Card/static/js/2.3d109e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0786e4f98849e46d47",
+    "url": "/Wedding-Invitation-Card/static/js/main.7a16a533.chunk.js"
+  },
+  {
+    "revision": "5a6a39a123b849716898",
+    "url": "/Wedding-Invitation-Card/static/js/runtime-main.23df98f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2325839878f2a016d89f524925b386ce.js"
+  "/Wedding-Invitation-Card/precache-manifest.ec1ce924ab63570d7374003470e33771.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Wedding-Invitation-Card/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
