d295761bffab56fdce8960b30605e4031622e9be
Will <rlagmlckd@gmail.com>
2020-11-26 22:12:10 +0900

    날짜 5일에서 4일로 수정

--- src/component/Title.js
+++ src/component/Title.js
@@ -7,7 +7,7 @@ class Title extends Component {
     return (
       <section className="flexbox title-container">
         <h2 className="flexbox title-container-date">
-          <span>12</span><span>⏤</span><span>05</span>
+          <span>12</span><span>⏤</span><span>04</span>
         </h2>
 
         <img className="main-img" src={logo} alt="flower"/>
@@ -16,7 +16,7 @@ class Title extends Component {
           <span className="text-xl text-bold color-gray">김희창</span> 그리고 <span className="text-xl text-bold color-gray">윤정인</span>
         </p>
 
-        <p className="title-container-details">오랜 기다림 끝에 <br/> 2020년 12월 05일<br/> 결혼합니다</p>
+        <p className="title-container-details">오랜 기다림 끝에 <br/> 2020년 12월 04일<br/> 결혼합니다</p>
       </section>
     );
   }
